e7a14dfad35e9c502c3f327888e132a9538e8d60
Gautham <gautham.oct3@gmail.com>
2021-07-29 15:17:47 +0530

    useRef

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
-import React, { useState, useMemo } from "react";
+import React, { useEffect, useRef } from "react";
 
 function App() {
-	const [counterOne, setCounterOne] = useState(0);
-    const [counterTwo, setCounterTwo] = useState(0);
-    
-    const incrementCounterOne = () => {    
-        setCounterOne(counterOne+1);
-    }
+    const inputRef = useRef();
 
-    const incrementCounterTwo = () => {
-        setCounterTwo(counterTwo+ 1);
-    }
+    useEffect(
+        () => inputRef.current.focus()
+        , []
+    )
 
-    const isEven = useMemo (() => {
-        var i = 0;
-        while(i<1000000000) {
-            i++;
-        }
-        return counterOne % 2 === 0 ? 'Even' : 'Odd';
-    },[counterOne])
-    
     return (
         <>
-        <div>
-            <button onClick={incrementCounterOne}> Counter One - {counterOne} </button>
-            <span> {isEven}</span>
-        </div>      
-        <div>
-            <button onClick={incrementCounterTwo}> Counter Two - {counterTwo} </button>
-        </div>
+            <input type="text" ref={inputRef} />
         </>
     )
 }
