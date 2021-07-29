32589a1c3a695ea17f8c51b7104689c48ca1a5cd
Gautham <gautham.oct3@gmail.com>
2021-07-29 15:31:32 +0530

    useReducer - II

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { useReducer } from "react";
 
 function App() {
-    const initialState = 0;
+    const initialState = {
+        firstCounter: 0,
+        secondCounter: 0
+    };
 
     const reducer = (state, action) => {
-        switch (action) {
+        switch (action.type) {
             case 'increment':
-                return state + 1;
+                return { ...state, firstCounter: state.firstCounter + action.value };
             case 'decrement':
-                return state - 1;
+                return { ...state, firstCounter: state.firstCounter - action.value };
+            case 'increment2':
+                return { ...state, secondCounter: state.secondCounter + action.value };
+            case 'decrement2':
+                return { ...state, secondCounter: state.secondCounter - action.value };
             case 'reset':
                 return initialState;
             default:
@@ -20,10 +27,13 @@ function App() {
 
     return (
         <>
-            <div> Counter  {counter} </div>
-            <button onClick={() => dispatch("increment")}> Increment </button>
-            <button onClick={() => dispatch("decrement")}> Decrement </button>
-            <button onClick={() => dispatch("reset")}> Reset</button>
+            <div> First Counter  {counter.firstCounter} </div>
+            <div> Second Counter  {counter.secondCounter} </div>
+            <button onClick={() => dispatch({ type: 'increment', value: 1 })}> Increment </button>
+            <button onClick={() => dispatch({ type: 'decrement', value: 1 })}> Decrement </button>
+            <button onClick={() => dispatch({ type: 'reset' })}> Reset</button>
+            <button onClick={() => dispatch({ type: 'increment2', value: 1 })}> Increment 2 </button>
+            <button onClick={() => dispatch({ type: 'decrement2', value: 1 })}> Decrement 2</button>
         </>
     )
 }
