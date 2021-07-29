e31b85b44ae3141cee5257909d4e1cf7a048f69f
Gautham <gautham.oct3@gmail.com>
2021-07-29 13:41:00 +0530

    useEffect - as componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
-	const [counter, setCounter] = useState(0);
-	const [name, setName] = useState('')
+	const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
 
-	useEffect(
-		() => {
-			console.log('React Hooks - Updating ...')
-			document.title = `You clicked ${counter} times`
-		}, [counter]
-	);
+    const logMousePosition = (e) => {
+        console.log('Mouse Movement Capture')
+        setX(e.clientX);
+        setY(e.clientY);
+    }
 
-	const incrementCounter = () => {
-		setCounter(prevCount => prevCount + 1);
-	}
+    useEffect (
+        () => {
+            console.log('React Hooks - useEffect ...')
+            window.addEventListener('mousemove', logMousePosition);   
+        },[]
+    );
 
-	return (
-		<>
-			<input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-			<button onClick={incrementCounter}> Counter One - {counter} </button>
-		</>
-	)
+
+    return (
+        <>
+            <div> {x} - {y}</div>
+        </>
+    )
 }
 
 export default App;
